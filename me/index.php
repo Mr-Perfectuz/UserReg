bffae8745ea8cd4448188952eb322b65675816dd
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-01 20:10:44 +0500

    File has been updated

---
+++ me/index.php
@@ -0,0 +1,21 @@
+
+<?php 
+
+session_start();
+
+	if(!isset($_SESSION['userlogin'])){
+		header("Location: login.php");
+	}
+
+	if(isset($_GET['logout'])){
+		session_destroy();
+		unset($_SESSION);
+		header("Location: login.php");
+	}
+
+?>
+
+<p>Welcome to index</p>
+
+
+<a href="index.php?logout=true">Logout</a>
